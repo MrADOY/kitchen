f56edbf818959ff983c2ccf8117368745d944052
Skalour <marceau.hollertt@hotmail.fr>
2020-01-13 18:26:36 +0100

    RecetteDetails amélioré

---
+++ kitchen/URLImage.swift
@@ -0,0 +1,20 @@
+//
+//  URLImage.swift
+//  kitchen
+//
+//  Created by Marceau Hollertt on 13/01/2020.
+//
+
+import SwiftUI
+
+struct URLImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct URLImage_Previews: PreviewProvider {
+    static var previews: some View {
+        URLImage()
+    }
+}
