c4267dc55816160d6bb705e74c3fd0595d0dcb66
Skalour <marceau.hollertt@hotmail.fr>
2020-01-13 18:29:24 +0100

    RecetteDetails

--- kitchen/RecetteDetail.swift
+++ kitchen/RecetteDetail.swift
@@ -17,16 +17,49 @@ struct RecetteDetail: View {
         
         VStack(){
             HStack(alignment: .top){
-                CircleImage(url : recette.picture_url)
-                    .offset(y: -130)
                 
                 Text(recette.name)
-                    .font(.headline)
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    .offset(x: 0, y: -40)
+                
+                HStack(spacing: 3) {
+                    ForEach(0 ..< (recette.avis!)) { _ in
+                        Image(systemName: "star.fill")
+                            .padding(.leading)
+                            .imageScale(.medium)
+                            .foregroundColor(.yellow)
+                    }
+                }
+                
+                Spacer()
+                
+                HStack(alignment: .center){
                     
+                    Text("Durée : 45min")
+                    Text("Difficulté : ***")
+                    Text("Coût : €€€")
+                }
+            }
             
+            HStack(alignment: .top){
+                VStack(alignment: .leading){
+                    RecettesIngredients()
+                }
+                
+                Divider()
+                
+                VStack(alignment: .trailing){
+                    RecettesInstructions()
+                }
+            }
+            
+            HStack(alignment: .bottom, spacing: 50){
+                
                 Button(action: {
-                    self.userData.recettes[self.recette.id].isFavorite.toggle()
-                }) {
+                self.userData.recettes[self.recette.id].isFavorite.toggle()
+                    })
+                {
                     if self.userData.recettes[self.recette.id].isFavorite {
                         Image(systemName: "star.fill")
                             .foregroundColor(Color.yellow)
@@ -34,32 +67,97 @@ struct RecetteDetail: View {
                         Image(systemName: "star")
                             .foregroundColor(Color.gray)
                     }
-                    
                 }
                 
-                NavigationLink(destination: AVPlayerView(videoURL : URL(string: "https://www.radiantmediaplayer.com/media/bbb-360p.mp4")!)
-                       .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-                       .edgesIgnoringSafeArea(.all)) {
-                             Text("Video")
-                             }
-                
-            }
-            HStack{
-            PieChartView(data : [
-                DataCharts(num :56, categorie: "Lipides", color: Color.red),
-                DataCharts(num :34, categorie: "Protéines", color: Color.blue),
-                DataCharts(num :128, categorie: "Glucides", color: Color.green)
-            ], title: "Macronutriment", legend: "Legend")
-                VStack{
-                RecettesInstructions()
-    
+                Button(action: {
+                self.userData.recettes[self.recette.id].isFavorite.toggle()
+                    })
+                {
+                    if self.userData.recettes[self.recette.id].isFavorite {
+                        Image(systemName: "star.fill")
+                            .foregroundColor(Color.yellow)
+                    } else {
+                        Image(systemName: "star")
+                            .foregroundColor(Color.gray)
+                    }
                 }
+                
+                Button(action: {
+                self.userData.recettes[self.recette.id].isFavorite.toggle()
+                    })
+                {
+                    if self.userData.recettes[self.recette.id].isFavorite {
+                        Image(systemName: "star.fill")
+                            .foregroundColor(Color.yellow)
+                    } else {
+                        Image(systemName: "star")
+                            .foregroundColor(Color.gray)
+                    }
+                }
+                
+                Button(action: {
+                self.userData.recettes[self.recette.id].isFavorite.toggle()
+                    })
+                {
+                    if self.userData.recettes[self.recette.id].isFavorite {
+                        Image(systemName: "star.fill")
+                            .foregroundColor(Color.yellow)
+                    } else {
+                        Image(systemName: "star")
+                            .foregroundColor(Color.gray)
+                    }
+                }
+                
+                Spacer()
+                
+                    NavigationLink(destination: AVPlayerView(videoURL : URL(string: "https://www.radiantmediaplayer.com/media/bbb-360p.mp4")!)
+                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                        .edgesIgnoringSafeArea(.all)) {
+                            Text("Voir la recette en vidéo")
+                    }
             }
+            
+                        
+//
+//                HStack(){
+//                Button(action: {
+//                    self.userData.recettes[self.recette.id].isFavorite.toggle()
+//                }) {
+//                    if self.userData.recettes[self.recette.id].isFavorite {
+//                        Image(systemName: "star.fill")
+//                            .foregroundColor(Color.yellow)
+//                    } else {
+//                        Image(systemName: "star")
+//                            .foregroundColor(Color.gray)
+//                    }
+//
+//                }
+//
+//                NavigationLink(destination: AVPlayerView(videoURL : URL(string: "https://www.radiantmediaplayer.com/media/bbb-360p.mp4")!)
+//                       .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+//                       .edgesIgnoringSafeArea(.all)) {
+//                             Text("Video")
+//                             }
+//            }
+//
+//            HStack{
+//            PieChartView(data : [
+//                DataCharts(num :56, categorie: "Lipides", color: Color.red),
+//                DataCharts(num :34, categorie: "Protéines", color: Color.blue),
+//                DataCharts(num :128, categorie: "Glucides", color: Color.green)
+//            ], title: "Macronutriment", legend: "Legend")
+//
+//                RecettesInstructions()
+//
+//
+//            }
+            Spacer()
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+        .background(URLImage(url: recette.picture_url)
+        .edgesIgnoringSafeArea(.all))   //NE PAS TOUCHER CES 3 LIGNES
     }
-        
-        
-    }
-}
+
 
 struct RecettesInstructions: View {
     
@@ -71,4 +169,18 @@ struct RecettesInstructions: View {
         Text("Etape 4 : Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
         }
     }
+}
+    
+struct RecettesIngredients: View {
+    
+    var body: some View {
+        VStack(spacing: 1.0){
+        Text("Ingredient 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+        Text("Ingredient 2 : Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+        Text("Ingredient 3 : Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+        Text("Ingredient 4 : Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+        }
+    }
+}
+
 }
--- kitchen/URLImage.swift
+++ kitchen/URLImage.swift
@@ -6,15 +6,21 @@
 //
 
 import SwiftUI
+import KingfisherSwiftUI
+
 
 struct URLImage: View {
+    var url: String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        KFImage(URL(string: url)!)
+        .resizable()
+        .opacity(0.6)
     }
 }
 
 struct URLImage_Previews: PreviewProvider {
     static var previews: some View {
-        URLImage()
+        URLImage(url: "https://www.tripsavvy.com/thmb/71Li6SYzHcJS6_LoonDPjvhovO4=/3837x2158/smart/filters:no_upscale()/steak-frites-838971058-95a4df7771ca4538aaaa46a830d06081.jpg")
     }
 }
+
